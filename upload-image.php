41a23120d151c4de215d4bcbba13a2e4b88c7f51
Parwinder Singh <parwindersingh@techmarcos.com>
2024-03-19 15:09:04 +0530

    Png and jpeg files resized

--- upload-image.php
+++ upload-image.php
@@ -28,22 +28,31 @@ function image_upload_shortcode_handler( $atts ) {
 }
 add_shortcode( 'image_upload_form', 'image_upload_shortcode_handler' );
 // Function to resize image
-function resizeImage($sourceImage, $targetWidth, $targetHeight,$resized_upload_path) {
+function resizeImage($sourceImage, $targetWidth, $targetHeight,$resized_upload_path,$file_type) {
     // Get the dimensions of the original image
     list($sourceWidth, $sourceHeight) = getimagesize($sourceImage);
     
     // Create a new image with the target dimensions
     $targetImage = imagecreatetruecolor($targetWidth, $targetHeight);
-    
-    // Load the original image
-    $sourceImage = imagecreatefromjpeg($sourceImage); // Change this based on your image type
-    
-    // Resize the image
-    imagecopyresampled($targetImage, $sourceImage, 0, 0, 0, 0, $targetWidth, $targetHeight, $sourceWidth, $sourceHeight);
     $upload_dir = wp_upload_dir();
-            $upload_path = $upload_dir['path'] . '/resized_image.jpg' ;
+    if($file_type=='image/jpeg'){
+        // Load the original image
+        $sourceImage = imagecreatefromjpeg($sourceImage); // Change this based on your image type
+        
+        // Resize the image
+        imagecopyresampled($targetImage, $sourceImage, 0, 0, 0, 0, $targetWidth, $targetHeight, $sourceWidth, $sourceHeight);
+        imagejpeg($targetImage, $resized_upload_path); // Change the file name and extension based on your needs
+     }
+    if($file_type=='image/png'){
+        // Load the original image
+        $sourceImage = imagecreatefrompng($sourceImage); // Change this based on your image type
+        
+        // Resize the image
+        imagecopyresampled($targetImage, $sourceImage, 0, 0, 0, 0, $targetWidth, $targetHeight, $sourceWidth, $sourceHeight);
+        imagepng($targetImage, $resized_upload_path); // Change the file name and extension based on your needs
+    }
+   
     // Save the resized image to a file
-    imagejpeg($targetImage, $resized_upload_path); // Change the file name and extension based on your needs
     
     // Free up memory
     imagedestroy($targetImage);
@@ -52,34 +61,39 @@ function resizeImage($sourceImage, $targetWidth, $targetHeight,$resized_upload_p
 function handle_image_upload() {
     if ( isset( $_FILES['image'] ) ) {
         $file = $_FILES['image'];
+        $file_type= $file['type'];
+        if($file_type=='image/jpeg' || $file_type=='image/png'){
 
+            if ( $file['error'] === UPLOAD_ERR_OK ) {
+                $file_name=time().'.jpg';
+                $sourceImage = $file['tmp_name']; // Path to the original image
+                $targetWidth = 300; // Desired width
+                $targetHeight = 200; // Desired height
+                $upload_dir = wp_upload_dir();
+                $upload_path = $upload_dir['path'] . '/' . $file_name;
+                $resized_upload_path = $upload_dir['path'] . '/resized' . $file_name;
+                $file_type= $file['type'];
+                resizeImage($sourceImage, $targetWidth, $targetHeight,$resized_upload_path,$file_type);
 
-        // Check for errors
-        if ( $file['error'] === UPLOAD_ERR_OK ) {
-            $file_name=time().'.jpg';
-            $sourceImage = $file['tmp_name']; // Path to the original image
-            $targetWidth = 300; // Desired width
-            $targetHeight = 200; // Desired height
-            $upload_dir = wp_upload_dir();
-            $upload_path = $upload_dir['path'] . '/' . $file_name;
-            $resized_upload_path = $upload_dir['path'] . '/resized' . $file_name;
-
-            resizeImage($sourceImage, $targetWidth, $targetHeight,$resized_upload_path);
-
-           
-
-            // Move the uploaded file to the WordPress uploads directory
-            move_uploaded_file( $file['tmp_name'], $upload_path );
-
-            // Generate shortcode for the uploaded image
-            $shortcode = '[image_shortcode url="' . $upload_dir['url'] . '/' . $file_name . '"]';
-
-            // Output the shortcode
-            echo '<img src="' . $upload_dir['url'] . '/resized' . $file_name . '" ><br><img src="' . $upload_dir['url'] . '/' . $file_name . '" >';
             
-        } else {
-            echo '<p>Error uploading image. Please try again.</p>';
-        }
+
+                // Move the uploaded file to the WordPress uploads directory
+                move_uploaded_file( $file['tmp_name'], $upload_path );
+
+                // Generate shortcode for the uploaded image
+                $shortcode = '[image_shortcode url="' . $upload_dir['url'] . '/' . $file_name . '"]';
+
+                // Output the shortcode
+                echo '<img src="' . $upload_dir['url'] . '/resized' . $file_name . '" ><br><img src="' . $upload_dir['url'] . '/' . $file_name . '" >';
+                
+            } else {
+                echo '<p>Error uploading image. Please try again.</p>';
+            }
+
+         }else{
+            echo '<p>Please select jpeg and png file and less then 1mb size</p>';
+         }
+        // Check for errors
     }
 }
 add_action( 'init', 'handle_image_upload' );
